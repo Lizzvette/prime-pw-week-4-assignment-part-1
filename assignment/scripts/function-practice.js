7a42cc55ec08258d64d91f13c31880b5358b596f
Lizzie Q <lizzieqtech@gmail.com>
2022-02-21 11:40:05 -0600

    WK4PT1-Corrected&Updated-Assignment

--- assignment/scripts/function-practice.js
+++ assignment/scripts/function-practice.js
@@ -15,17 +15,14 @@ console.log('Test - should say "Hello World!"', hello());
 // 2. Function to return an personalized hello, using the `name` argument.
 //    for example 'Hello, Jo!', or 'Hello, Stacy!'
 function helloName( name ) {
-  return 'Hello, Sonny!';
+  return `Hello,  ${name}`;
 }
-// Remember to call the function to test
-console.log('#2 should: say "Hello, Sonny!"', helloName(name)); 
-  
-/*OR 
+console.log(helloName('Cameron'));
 
-function helloName( name ){
-  console.log( name );
-}  
-helloName('Hello, Sonny!');*/
+// Remember to call the function to test
+//console.log('#2 should: say "Hello, Sonny!"', helloName(name)); 
+  
+
 
 // 3. Function to add two numbers together & return the result
 function addNumbers( firstNumber, secondNumber ) {
@@ -62,10 +59,13 @@ console.log( 'isPositive - should say false', isPositive(-3) );
 //    array is empty, return `undefined`.
 const arrayArt = ['blown-glass', 'acrylic painting', 'sculpture'];
 function getLast( arrayArt ) {      // arrayArt is the parameter
+  if(arrayArt.length > 0){
   return arrayArt[arrayArt.length-1];
 }
-console.log( 'getLast - should return: ', getLast( arrayArt));// outside function is the argument
-
+return undefined;
+}
+console.log( 'getLast - should return: ', getLast([1,2,3]));// outside function is the argument
+console.log ('should return: ', getLast([]));
 // 7. Function to find a value in an array. Return true if the 
 //    value is found and false otherwise. Use a loop;
 //    DO NOT use Array.includes, Array.indexOf, or Array.find 
@@ -77,45 +77,77 @@ console.log( 'getLast - should return: ', getLast( arrayArt));// outside functio
 const anyThing = getSomething(5);
 console.log(anyThing); */
 
-let array = [5,2,7,10,58];
+//let array = [5,2,7,10,58];
 
 function find( value, array ){
 for (let i = 0; i < array.length; i++)  {
-  if (value === array){
+  if (array[i] === value){
     return true;
   }
+}
   return false;
 }
-}
-console.log(array);
-console.log(find);
-console.log(find(5, [2,7,10,58]));
+
+console.log(find (2 ,[2,7,10,58]));
+console.log(find (5, [2,7,10,]));
 
 
-
-// Stretch Goals
+// Stretch Goals -  MDN website
 // ----------------------
 // 8. Function to check if a letter is the first letter in a 
 //    string. Return true if it is, and false otherwise
-function isFirstLetter(letter, string) {
 
+function isFirstLetter(letter, string) {
+  
+    if (letter === string.charAt(0)) {
+      return true;
+    }
+  
+return false;
 }
 console.log( 'isFirstLetter - should say true', isFirstLetter('a', 'apple') );
 console.log( 'isFirstLetter - should say false', isFirstLetter('z', 'apple') );
 
-// 9. Function to return the sum of all numbers in an array
-function sumAll( ) {
-  let sum = 0
-  // TODO: loop to add items
-  return sum;
-}
 
+
+// 9. Function to return the sum of all numbers in an array
+const previousValue = [5, 6, 7, 8,];
+/*const currentValue = [];
+let numbers = [5, 6, 7, 8].reduce(function (previousValue, currentValue){
+return previousValue + currentValue
+}, 0)
+console.log ( sumAll(  ));*/
+
+let sum = 0;
+for (let i = 0; i < previousValue.length; i++){
+  sum += previousValue[i];
+}
+console.log("Sum is " + sum)
 // 10. Function to return a new array of all positive (greater than zero)
 //     numbers contained in an input array. If there are no positive numbers
 //     return an empty array. Note: The input array should not change.
-
-
+function theArray(positiveArray){
+  let firstArray= [];
+  for(let i=0;i<positiveArray.length; i++){
+    if(positiveArray[i]>0)
+    firstArray.push(positiveArray[i]);
+  }
+  return firstArray;
+}
+console.log(theArray([7,2,5]));
+console.log(theArray([-7,-2,-5]));
+  
 
 // 11. Pick a problem from Edabit(https://edabit.com/) or 
 //     CodeWars(https://www.codewars.com/). Then describe it 
 //     here in a comment, write the function, and test it!
+// Comment: Define function name circuitPower with 2 parameters voltage, current. 
+// Next create a variable to take the task (calculation). Then return variable (power) that will also end function block.
+//Lastly, to evoke function must call with function declaration/name. Add within console.log with argument to pass back to function.
+
+function circuitPower(voltage, current) {
+	let power =  voltage * current;
+  return power;
+}
+console.log ('The power is:', circuitPower(200,5 ));
+
